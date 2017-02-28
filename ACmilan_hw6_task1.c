ab55654e056862d96c194c5e84cd530173a82784
Chris Werner <chris.lex.werner@gmail.com>
2017-02-28 17:12:13 +0000

    Inital Commit, file creating

---
+++ ACmilan_hw6_task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ACmilan_hw6_task1.c
+ *       	Usage:  ./ACmilan_hw6_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2017 05:11:42 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Chris Werner (), christopherwerner@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>		/* For Standard Library*/
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
