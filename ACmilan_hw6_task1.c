7088b6b57cb04c65b1af68f8cb651b1e06bbf284
Chris Werner <chris.lex.werner@gmail.com>
2017-03-01 06:46:42 +0000

    Structure of Main function added. Usage adn AskQuesiton funcitons added and funcitoning

--- ACmilan_hw6_task1.c
+++ ACmilan_hw6_task1.c
@@ -15,15 +15,77 @@
 #include <stdlib.h>		/* For Standard Library*/
 
 /* Function Prototypes */
+void Usage(void);
+int AskQuestion(void);
 
 /* Main Program */
 int main(int argc, char *argv[])
 {
-
+	/*  Verify User Input */
+	if (argc != 3)
+	{
+		Usage();
+	}
+	else
+	{
+		/*  Declare and initialize variables */
+		/*  Convert two user inputs to float  */
+		float x = atof(argv[1]);
+		float y = atof(argv[2]);
+		/*  Validate arguments are not characters or zeros */
+		if (x == 0 || y == 0)
+		{
+			Usage();
+		}
+		else
+		{
+			/*  Calculate polar coordinates, Call Polar() 
+			 *  and Call Showit() */
+			/* Call AskQuestion() */
+			AskQuestion();
+		}
+	}
 	return 0;
 }
 
 
 /* Function Defenitions */
+void Usage(void)
+{
+	printf("Usage: Please enter two parameters.\n");
+	printf("Usage: Parameters must be non-zero floating points.\n");
+	return;
+}
+int AskQuestion(void)
+{
+	/* Begin Loop for performing more calculations */
+	while (1)	
+	{
+		/*  Declare variable for user input */
+		int cont;
+		char catch;
+		/*  Prompt user if they would like to continue */
+		printf("Would you like to perform another calculation (Y/N)?\n");
+		printf("Y = 1, N = 0: ");
+		/*  Scan users input. Second scan catches enter key*/
+		scanf("%d",&cont);
+		scanf("%c",&catch);
+		/*  If no more calcs, exit loop */
+		if (cont == 0)
+		{
+			break;
+		}
+		/*  If more calcs, call GetRec(), Polar(), and Showit() */
+		else if (cont == 1)
+		{
 
+		}
+		/*  If input is not one or zero, inform user */
+		else
+		{
+			printf("Bad input, try again. \n");
+		}
+	}
+	return 0;
+}
 
